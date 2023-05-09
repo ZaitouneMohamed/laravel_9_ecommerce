76bb39965c712ab84f090ffc7f3ebede3dc4ab72
mohamed zaitoune <115373788+ZaitouneMohamed@users.noreply.github.com>
2023-05-09 21:19:24 +0100

    product model + display it in index to add i=them in cart

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        "title", "slug", "description",
+        "price", "old_price",
+        "image", "inStock", "sub_categories_id"
+    ];
+    public function SubCategorie()
+    {
+        return $this->belongsTo(SubCategorie::class);
+    }
 }
