903a943788978491020bd8fb5daf5d0171975241
mohamed zaitoune <115373788+ZaitouneMohamed@users.noreply.github.com>
2023-07-26 23:08:09 +0100

    Update Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,7 +16,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('command:test')->everyThreeMinutes();
+        $schedule->command('command:test')->everyMinute();
     }
 
     /**
