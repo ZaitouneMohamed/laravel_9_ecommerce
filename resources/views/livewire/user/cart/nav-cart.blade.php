ce13f64576c0719eaf46df8021d01b1bae3a8b21
mohamed zaitoune <115373788+ZaitouneMohamed@users.noreply.github.com>
2023-07-29 00:33:57 +0100

    Update nav-cart.blade.php

--- resources/views/livewire/user/cart/nav-cart.blade.php
+++ resources/views/livewire/user/cart/nav-cart.blade.php
@@ -1,17 +1,20 @@
 <div wire:poll>
-    @foreach (session('cart') as $id => $item)
-        <div class="single-cart-item">
-            <a href="#" class="product-image">
-                <img src="{{ asset('assets/landing/img/product-img/product-1.jpg') }}" class="cart-thumb" alt="">
-                <!-- Cart Item Desc -->
-                <div class="cart-item-desc">
-                    <span class="product-remove" wire:click="DeleteItem({{ $item['id'] }})"><i class="fa fa-close"
-                            aria-hidden="true"></i></span>
-                    <span class="badge">{{ Str::limit($item['title'], 10, '...') }}</span>
-                    <h6>{{ Str::limit($item['title'], 10, '...') }}</h6>
-                    <p class="price">${{ $item['price'] }}</p>
-                </div>
-            </a>
-        </div>
-    @endforeach
+    @if (session('cart'))
+        @foreach (session('cart') as $id => $item)
+            <div class="single-cart-item">
+                <a href="#" class="product-image">
+                    <img src="{{ asset('assets/landing/img/product-img/product-1.jpg') }}" class="cart-thumb"
+                        alt="">
+                    <!-- Cart Item Desc -->
+                    <div class="cart-item-desc">
+                        <span class="product-remove" wire:click="DeleteItem({{ $item['id'] }})"><i class="fa fa-close"
+                                aria-hidden="true"></i></span>
+                        <span class="badge">{{ Str::limit($item['title'], 10, '...') }}</span>
+                        <h6>{{ Str::limit($item['title'], 10, '...') }}</h6>
+                        <p class="price">${{ $item['price'] }}</p>
+                    </div>
+                </a>
+            </div>
+        @endforeach
+    @endif
 </div>
