7d45417dc7a3a5ece99f5489e6c35f39c8d4b3b4
mohamed zaitoune <115373788+ZaitouneMohamed@users.noreply.github.com>
2023-09-07 19:53:25 +0100

    Update cart-nav-bar.blade.php

--- resources/views/livewire/cart/cart-nav-bar.blade.php
+++ resources/views/livewire/cart/cart-nav-bar.blade.php
@@ -1,4 +1,4 @@
-<div wire:poll.750ms>
+<div wire:poll>
     <li class="col pr-xl-0 px-2 px-sm-3" >
         <a href="../shop/cart.html" class="text-gray-90 position-relative d-flex " data-toggle="tooltip" data-placement="top"
             title="Cart">
@@ -6,7 +6,7 @@
             <span
                 class="bg-lg-down-black width-22 height-22 bg-primary position-absolute d-flex align-items-center justify-content-center rounded-circle left-12 top-8 font-weight-bold font-size-12">{{ $cartcount }}</span>
             <span
-                class="d-none d-xl-block font-weight-bold font-size-16 text-gray-90 ml-3">${{ $prix  }}</span>
+                class="d-none d-xl-block font-weight-bold font-size-16 text-gray-90 ml-3">${{ now()  }}</span>
         </a>
     </li>
 
