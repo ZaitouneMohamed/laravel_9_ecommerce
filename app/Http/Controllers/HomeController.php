c8a637e90afe20b575e75ffab09e78b8c1092883
mohamed zaitoune <115373788+ZaitouneMohamed@users.noreply.github.com>
2023-09-08 08:55:07 +0100

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,16 +4,24 @@ namespace App\Http\Controllers;
 
 use App\Models\Categorie;
 use App\Models\Product;
+use App\Services\GetProducts;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
+    protected $ProductService;
+
+    public function __construct(GetProducts $product)
+    {
+        $this->ProductService = $product;
+    }
     public function Home()
     {
         $categories = Categorie::latest()->take(3)->get();
         $latest_products = Product::latest()->take(4)->get();
         $prenium_products = Product::latest()->Prenium()->take(4)->get();
-        return view('electro.index', compact("latest_products", "categories", "prenium_products"));
+        $topcategorie = $this->ProductService->GetTopCategorie();
+        return view('electro.index', compact("latest_products", "categories", "prenium_products", "topcategorie"));
     }
 
     public function checkout()
