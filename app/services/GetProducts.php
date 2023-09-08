6195e38da7441e7932c45271246713810ae5426b
mohamed zaitoune <115373788+ZaitouneMohamed@users.noreply.github.com>
2023-09-08 08:55:03 +0100

    get the top categorie with most products

--- app/services/GetProducts.php
+++ app/services/GetProducts.php
@@ -2,7 +2,9 @@
 
 namespace App\Services;
 
+use App\Models\Categorie;
 use App\Models\Product;
+use App\Models\SubCategorie;
 
 class GetProducts
 {
@@ -17,5 +19,15 @@ class GetProducts
             $products = Product::where('categorie_id', $id)->paginate(20);
             return $products;
         }
+    }
+
+    public function GetTopCategorie()
+    {
+        $SubCategorie = SubCategorie::withCount('products')
+            ->orderByDesc('products_count')
+            ->with("products")
+            ->first();
+        // $categoryWithMostProduct = $SubCategorie->categorie;
+        return $SubCategorie;
     }
 }
