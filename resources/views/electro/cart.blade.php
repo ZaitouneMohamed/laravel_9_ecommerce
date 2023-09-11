3384ab46f64faf5f09bc56316ad38dad42def946
mohamed zaitoune <115373788+ZaitouneMohamed@users.noreply.github.com>
2023-09-11 19:57:46 +0100

    Update cart.blade.php

--- resources/views/electro/cart.blade.php
+++ resources/views/electro/cart.blade.php
@@ -24,182 +24,127 @@
         <div class="container">
             <div class="row">
                 <div class="col-lg-12">
-                    <!-- Products-List-Wrapper -->
-                    <div class="table-wrapper u-s-m-b-60">
-                        <table>
-                            <thead>
-                                <tr>
-                                    <th>Product</th>
-                                    <th>Price</th>
-                                    <th>Quantity</th>
-                                    <th>total</th>
-                                    <th>Subtotal</th>
-                                </tr>
-                            </thead>
-                            <tbody>@php $total = 0 @endphp
-                                @foreach (session('cart') as $id => $item)
-                                    @php
-                                        $total += $item['price'] * $item['quantity'];
-                                    @endphp
-                                    <tr class="delete-product">
-                                        <td>
-                                            <div class="cart-anchor-image">
-                                                <a href="single-product.html">
-                                                    <img src="{{ $item['image'] }}" alt="Product">
-                                                    <h6>{{ $item['title'] }}</h6>
-                                                </a>
-                                            </div>
-                                        </td>
-                                        <td>
-                                            <div class="cart-price" id="prix">
-                                                ${{ $item['price'] }}
-                                            </div>
-                                        </td>
-                                        <td>
-                                            <div class="cart-quantity">
-                                                <div class="quantity">
-                                                    <input type="text" class="quantity-text-field"
-                                                        id="{{ $item['id'] }}" value="{{ $item['quantity'] }}"
-                                                        oninput="edit({{ $item['id'] }})">
-                                                    <a class="plus-a" data-max="1000">&#43;</a>
-                                                    <a class="minus-a" data-min="1">&#45;</a>
-                                                </div>
-                                            </div>
-                                        </td>
-                                        <td>
-                                            <div class="cart-total">
-                                                <span>
-                                                    {{ $item['price'] * $item['quantity'] }}
-                                                </span>
-                                            </div>
-                                        </td>
-                                        <td>
-                                            <div class="action-wrapper">
-                                                <button class="button button-outline-secondary fas fa-sync"></button>
-                                                <button class="button button-outline-secondary fas fa-trash"
-                                                    onclick="deleteProduct({{ $item['id'] }})"></button>
-                                            </div>
-                                        </td>
-                                    </tr>
-                                @endforeach
-                            </tbody>
-                        </table>
-                    </div>
-                    <!-- Products-List-Wrapper /- -->
-                    <!-- Coupon -->
-                    <div class="coupon-continue-checkout u-s-m-b-60">
-                        <div class="coupon-area">
-                            <h6>Enter your coupon code if you have one.</h6>
-                            <div class="coupon-field">
-                                <label class="sr-only" for="coupon-code">Apply Coupon</label>
-                                <input id="coupon-code" type="text" class="text-field" placeholder="Coupon Code">
-                                <button type="submit" class="button">Apply Coupon</button>
-                            </div>
-                        </div>
-                        <div class="button-area">
-                            <a href="shop-v1-root-category.html" class="continue">Continue Shopping</a>
-                            <a href="checkout.html" class="checkout">Proceed to Checkout</a>
-                        </div>
-                    </div>
-                    <!-- Coupon /- -->
-                    <!-- Billing -->
-                    <div class="calculation u-s-m-b-60">
-                        <div class="table-wrapper-2">
+                    @if (session('cart'))
+                        <!-- Products-List-Wrapper -->
+                        <div class="table-wrapper u-s-m-b-60">
                             <table>
                                 <thead>
                                     <tr>
-                                        <th colspan="2">Cart Totals</th>
+                                        <th>Product</th>
+                                        <th>Price</th>
+                                        <th>Quantity</th>
+                                        <th>total</th>
+                                        <th>Subtotal</th>
                                     </tr>
                                 </thead>
-                                <tbody>
-                                    <tr>
-                                        <td>
-                                            <h3 class="calc-h3 u-s-m-b-0">Subtotal</h3>
-                                        </td>
-                                        <td>
-                                            <span class="calc-text">$222.00</span>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td>
-                                            <h3 class="calc-h3 u-s-m-b-8">Shipping</h3>
-                                            <div class="calc-choice-text u-s-m-b-4">Flat Rate: Not Available</div>
-                                            <div class="calc-choice-text u-s-m-b-4">Free Shipping: Not Available</div>
-                                            <a data-toggle="collapse" href="#shipping-calculation"
-                                                class="calc-anchor u-s-m-b-4">Calculate Shipping
-                                            </a>
-                                            <div class="collapse" id="shipping-calculation">
-                                                <form>
-                                                    <div class="select-country-wrapper u-s-m-b-8">
-                                                        <div class="select-box-wrapper">
-                                                            <label class="sr-only" for="select-country">Choose your country
-                                                            </label>
-                                                            <select class="select-box" id="select-country">
-                                                                <option selected="selected" value="">Choose your
-                                                                    country...
-                                                                </option>
-                                                                <option value="">United Kingdom (UK)</option>
-                                                                <option value="">United States (US)</option>
-                                                                <option value="">United Arab Emirates (UAE)</option>
-                                                            </select>
-                                                        </div>
+                                <tbody>@php $total = 0 @endphp
+                                    @foreach (session('cart') as $id => $item)
+                                        @php
+                                            $total += $item['price'] * $item['quantity'];
+                                        @endphp
+                                        <tr class="delete-product">
+                                            <td>
+                                                <div class="cart-anchor-image">
+                                                    <a href="single-product.html">
+                                                        <img src="{{ $item['image'] }}" alt="Product">
+                                                        <h6>{{ $item['title'] }}</h6>
+                                                    </a>
+                                                </div>
+                                            </td>
+                                            <td>
+                                                <div class="cart-price" id="prix">
+                                                    ${{ $item['price'] }}
+                                                </div>
+                                            </td>
+                                            <td>
+                                                <div class="cart-quantity">
+                                                    <div class="quantity">
+                                                        <input type="text" class="quantity-text-field"
+                                                            id="{{ $item['id'] }}" value="{{ $item['quantity'] }}"
+                                                            oninput="edit({{ $item['id'] }})">
+                                                        <a class="plus-a" data-max="1000">&#43;</a>
+                                                        <a class="minus-a" data-min="1">&#45;</a>
                                                     </div>
-                                                    <div class="select-state-wrapper u-s-m-b-8">
-                                                        <div class="select-box-wrapper">
-                                                            <label class="sr-only" for="select-state">Choose your state
-                                                            </label>
-                                                            <select class="select-box" id="select-state">
-                                                                <option selected="selected" value="">Choose your
-                                                                    state...
-                                                                </option>
-                                                                <option value="">Alabama</option>
-                                                                <option value="">Alaska</option>
-                                                                <option value="">Arizona</option>
-                                                            </select>
-                                                        </div>
-                                                    </div>
-                                                    <div class="town-city-div u-s-m-b-8">
-                                                        <label class="sr-only" for="town-city"></label>
-                                                        <input type="text" id="town-city" class="text-field"
-                                                            placeholder="Town / City">
-                                                    </div>
-                                                    <div class="postal-code-div u-s-m-b-8">
-                                                        <label class="sr-only" for="postal-code"></label>
-                                                        <input type="text" id="postal-code" class="text-field"
-                                                            placeholder="Postcode / Zip">
-                                                    </div>
-                                                    <div class="update-totals-div u-s-m-b-8">
-                                                        <button class="button button-outline-platinum">Update
-                                                            Totals</button>
-                                                    </div>
-                                                </form>
-                                            </div>
-                                        </td>
-                                        <td>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td>
-                                            <h3 class="calc-h3 u-s-m-b-0" id="tax-heading">Tax</h3>
-                                            <span> (estimated for your country)</span>
-                                        </td>
-                                        <td>
-                                            <span class="calc-text">$0.00</span>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td>
-                                            <h3 class="calc-h3 u-s-m-b-0">Total</h3>
-                                        </td>
-                                        <td>
-                                            <span class="calc-text">$220.00</span>
-                                        </td>
-                                    </tr>
+                                                </div>
+                                            </td>
+                                            <td>
+                                                <div class="cart-total">
+                                                    <span>
+                                                        {{ $item['price'] * $item['quantity'] }}
+                                                    </span>
+                                                </div>
+                                            </td>
+                                            <td>
+                                                <div class="action-wrapper">
+                                                    <button class="button button-outline-secondary fas fa-sync"></button>
+                                                    <button class="button button-outline-secondary fas fa-trash"
+                                                        onclick="deleteProduct({{ $item['id'] }})"></button>
+                                                </div>
+                                            </td>
+                                        </tr>
+                                    @endforeach
                                 </tbody>
                             </table>
                         </div>
-                    </div>
-                    <!-- Billing /- -->
+                        <!-- Products-List-Wrapper /- -->
+                        <!-- Coupon -->
+                        <div class="coupon-continue-checkout u-s-m-b-60">
+                            <div class="coupon-area">
+                                <h6>Enter your coupon code if you have one.</h6>
+                                <div class="coupon-field">
+                                    <label class="sr-only" for="coupon-code">Apply Coupon</label>
+                                    <input id="coupon-code" type="text" class="text-field" placeholder="Coupon Code">
+                                    <button type="submit" class="button">Apply Coupon</button>
+                                </div>
+                            </div>
+                            <div class="button-area">
+                                <a href="shop-v1-root-category.html" class="continue">Continue Shopping</a>
+                                <a href="{{ route('cart.checkout') }}" class="checkout">Proceed to Checkout</a>
+                            </div>
+                        </div>
+                        <!-- Coupon /- -->
+                        <!-- Billing -->
+                        <div class="calculation u-s-m-b-60">
+                            <div class="table-wrapper-2">
+                                <table>
+                                    <thead>
+                                        <tr>
+                                            <th colspan="2">Cart Totals</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>
+                                                <h3 class="calc-h3 u-s-m-b-0">Subtotal</h3>
+                                            </td>
+                                            <td>
+                                                <span class="calc-text">${{ $total }}</span>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>
+                                                <h3 class="calc-h3 u-s-m-b-0" id="tax-heading">Delivery Frais</h3>
+                                            </td>
+                                            <td>
+                                                <span class="calc-text">$30.00</span>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>
+                                                <h3 class="calc-h3 u-s-m-b-0">Total</h3>
+                                            </td>
+                                            <td>
+                                                <span class="calc-text">${{ $total + 30 }}</span>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                        <!-- Billing /- -->
+                    @else
+                        <h1 class="text text-center">Your Cart Is Empty</h1>
+                    @endif
                 </div>
             </div>
         </div>
