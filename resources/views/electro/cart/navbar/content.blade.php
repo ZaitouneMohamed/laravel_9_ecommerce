6ecfa5539aea2396c57150b9a5777c5073ae1c4e
mohamed zaitoune <115373788+ZaitouneMohamed@users.noreply.github.com>
2023-10-11 17:53:36 +0100

    Update content.blade.php

--- resources/views/electro/cart/navbar/content.blade.php
+++ resources/views/electro/cart/navbar/content.blade.php
@@ -4,15 +4,12 @@
             YOUR CART
             <button type="button" class="button ion ion-md-close" id="mini-cart-close"></button>
         </div>
-        @if (session('cart'))
-            <ul class="mini-cart-list" id="cart_content">
-            </ul>
-            <div class="mini-shop-total clearfix">
-                <span class="mini-total-heading float-left">Total:</span>
-                <span class="mini-total-price float-right" id="total_content"><span>
-            </div>
-        @else
-        @endif
+        <ul class="mini-cart-list" id="cart_content">
+        </ul>
+        <div class="mini-shop-total clearfix">
+            <span class="mini-total-heading float-left">Total:</span>
+            <span class="mini-total-price float-right" id="total_content"><span>
+        </div>
         <div class="mini-action-anchors">
             <a href="{{ route('cart.list') }}" class="cart-anchor">View Cart</a>
             <a href="{{ route('cart.checkout') }}" class="checkout-anchor">Checkout</a>
