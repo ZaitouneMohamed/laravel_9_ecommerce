16b7c6e0d4ce71ccb2676e40f4721034f90bb73e
mohamed zaitoune <115373788+ZaitouneMohamed@users.noreply.github.com>
2023-10-18 21:11:56 +0100

    Update AllProducts.blade.php

--- resources/views/electro/AllProducts.blade.php
+++ resources/views/electro/AllProducts.blade.php
@@ -1,141 +1,168 @@
-@extends('home.master.master')
+@extends('electro.layouts.master')
 
 @section('content')
-    <div class="breadcumb_area bg-img" style="background-image: url(img/bg-img/breadcumb.jpg);">
-        <div class="container h-100">
-            <div class="row h-100 align-items-center">
-                <div class="col-12">
-                    <div class="page-title text-center">
-                        <h2>dresses {{$products->count()}} </h2>
-                    </div>
-                </div>
+    <!-- Page Introduction Wrapper -->
+    <div class="page-style-a">
+        <div class="container">
+            <div class="page-intro">
+                <h2>Shop</h2>
+                <ul class="bread-crumb">
+                    <li class="has-separator">
+                        <i class="ion ion-md-home"></i>
+                        <a href="home.html">Home</a>
+                    </li>
+                    <li class="is-marked">
+                        <a href="shop-v2-sub-category.html">Shop</a>
+                    </li>
+                </ul>
             </div>
         </div>
     </div>
-    <!-- ##### Breadcumb Area End ##### -->
-
-    <!-- ##### Shop Grid Area Start ##### -->
-    <section class="shop_grid_area section-padding-80">
+    <!-- Page Introduction Wrapper /- -->
+    <!-- Shop-Page -->
+    <div class="page-shop u-s-p-t-80">
         <div class="container">
+            <!-- Shop-Intro -->
+            <div class="shop-intro">
+                <ul class="bread-crumb">
+                    <li class="has-separator">
+                        <a href="home.html">Home</a>
+                    </li>
+                    <li class="has-separator">
+                        <a href="shop-v1-root-category.html">Men's Clothing</a>
+                    </li>
+                    <li class="is-marked">
+                        <a href="shop-v3-sub-sub-category.html">Tops</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- Shop-Intro /- -->
             <div class="row">
-                <div class="col-12 col-md-4 col-lg-3">
-                    <div class="shop_sidebar_area">
-                        <!-- ##### Single Widget ##### -->
-                        <div class="widget price mb-50">
-                            <!-- Widget Title -->
-                            <h6 class="widget-title mb-30">Filter by</h6>
-                            <!-- Widget Title 2 -->
-                            <p class="widget-title2 mb-30">Price</p>
-
-
-                            <div class="widget-desc">
-                                <div class="slider-range">
-                                    <div data-min="49" data-max="360" data-unit="$"
-                                        class="slider-range-price ui-slider ui-slider-horizontal ui-widget ui-widget-content ui-corner-all"
-                                        data-value-min="49" data-value-max="360" data-label-result="Range:">
-                                        <div class="ui-slider-range ui-widget-header ui-corner-all"></div>
-                                        <span class="ui-slider-handle ui-state-default ui-corner-all" tabindex="0"></span>
-                                        <span class="ui-slider-handle ui-state-default ui-corner-all" tabindex="0"></span>
-                                    </div>
-                                    <div class="range-price">Range: $49.00 - $360.00</div>
-                                </div>
+                <!-- Shop-Left-Side-Bar-Wrapper -->
+                <div class="col-lg-3 col-md-3 col-sm-12">
+                    <!-- Filter-Price -->
+                    <div class="facet-filter-by-price">
+                        <h3 class="title-name">Price</h3>
+                        <form class="facet-form" action="#" method="post">
+                            <!-- Final-Result -->
+                            <div class="amount-result clearfix">
+                                <div class="price-from">$0</div>
+                                <div class="price-to">$3000</div>
                             </div>
+                            <!-- Final-Result /- -->
+                            <!-- Range-Slider  -->
+                            <div class="price-filter"></div>
+                            <!-- Range-Slider /- -->
+                            <!-- Range-Manipulator -->
+                            <div class="price-slider-range" data-min="0" data-max="5000" data-default-low="0"
+                                data-default-high="3000" data-currency="$"></div>
+                            <!-- Range-Manipulator /- -->
+                            <button type="submit" class="button button-primary">Filter</button>
+                        </form>
+                    </div>
+                    <!-- Filter-Price /- -->
+                    <!-- Filters /- -->
+                </div>
+                <!-- Shop-Left-Side-Bar-Wrapper /- -->
+                <!-- Shop-Right-Wrapper -->
+                <div class="col-lg-9 col-md-9 col-sm-12">
+                    <!-- Page-Bar -->
+                    <div class="page-bar clearfix">
+                        <div class="shop-settings">
+                            <a id="list-anchor" class="active">
+                                <i class="fas fa-th-list"></i>
+                            </a>
+                            <a id="grid-anchor">
+                                <i class="fas fa-th"></i>
+                            </a>
                         </div>
                     </div>
-                </div>
-
-                <div class="col-12 col-md-8 col-lg-9">
-                    <div class="shop_grid_product_area">
-                        <div class="row">
-                            <div class="col-12">
-                                <div class="product-topbar d-flex align-items-center justify-content-between">
-                                    <!-- Total Products -->
-                                    <div class="total-products">
-                                        <p><span>186</span> products found</p>
+                    <!-- Page-Bar /- -->
+                    <!-- Row-of-Product-Container -->
+                    <div class="row product-container list-style">
+                        @forelse ($products as $item)
+                            <div class="product-item col-lg-4 col-md-6 col-sm-6">
+                                <div class="item">
+                                    <div class="image-container">
+                                        <a class="item-img-wrapper-link" href="{{ route('products.show', $item->id) }}">
+                                            <img class="img-fluid" src="{{ $item->FirstImage }}" alt="Product">
+                                        </a>
+                                        <div class="item-action-behaviors">
+                                            <a class="item-quick-look" data-toggle="modal" href="#quick-view">Quick Look</a>
+                                            @auth
+                                                <a class="item-addwishlist" href="javascript:void(0)">Add to Wishlist</a>
+                                            @endauth
+                                            <a class="item-addCart" href="javascript:void(0)">Add to Cart</a>
+                                        </div>
                                     </div>
-                                    <!-- Sorting -->
-                                    <div class="product-sorting d-flex">
-                                        <p>Sort by:</p>
-                                        <form action="#" method="get">
-                                            <select name="select" id="sortByselect">
-                                                <option value="value">Highest Rated</option>
-                                                <option value="value">Newest</option>
-                                                <option value="value">Price: $$ - $</option>
-                                                <option value="value">Price: $ - $$</option>
-                                            </select>
-                                            <input type="submit" class="d-none" value="">
+                                    <div class="item-content">
+                                        <div class="what-product-is">
+                                            <ul class="bread-crumb">
+                                                <li>
+                                                    <a href="#">{{ $item->SubCategorie->name }}</a>
+                                                </li>
+                                            </ul>
+                                            <h6 class="item-title">
+                                                <a href="{{ route('products.show', $item->id) }}">{{ $item->title }}</a>
+                                            </h6>
+                                            <div class="item-description">
+                                                <p>
+                                                    {{ $item->description }}
+                                                </p>
+                                            </div>
+                                        </div>
+                                        <div class="price-template">
+                                            <div class="item-new-price">
+                                                ${{ $item->price }}
+                                            </div>
+                                            <div class="item-old-price">
+                                                ${{ $item->old_price }}
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="tag new">
+                                        <span>NEW</span>
+                                    </div>
+                                </div>
+                            </div>
+                        @empty
+                            <!-- Shop-Page -->
+                            <div class="page-shop u-s-p-t-80">
+                                <div class="container">
+                                    <!-- Result-Wrapper -->
+                                    <div class="result-wrapper u-s-p-y-80">
+                                        <ul class="bread-crumb">
+                                            <li class="has-separator">
+                                                <a href="{{ route('home') }}">Home</a>
+                                            </li>
+                                            <li class="is-marked">
+                                                <a href="#">All Categories</a>
+                                            </li>
+                                        </ul>
+                                        <h4>Your search, did not match any products. A partial match of your keywords is
+                                            listed below.</h4>
+                                        <h1>SORRY</h1>
+                                        <form action="{{ route('Search') }}" method="POST">
+                                            @csrf
+                                            @method('GET')
+                                            <label class="sr-only" for="search-not-found">Enter Keywords</label>
+                                            <input type="text" class="text-field" id="search-not-found" name="word"
+                                                placeholder="Search Products...">
+                                            <button class="button">Search</button>
                                         </form>
                                     </div>
+                                    <!-- Result-Wrapper /- -->
                                 </div>
                             </div>
-                        </div>
-
-                        <div class="row">
-
-                            @foreach ($products as $item)
-                                <div class="col-12 col-sm-6 col-lg-4">
-                                    <div class="single-product-wrapper">
-                                        <!-- Product Image -->
-                                        <div class="product-img">
-                                            <img src="{{ asset('images/products') }}/{{ $item->images->first()->url }}" alt="">
-                                            <!-- Hover Thumb -->
-                                            <img class="hover-img" src="{{ asset('images/products') }}/{{ $item->images->last()->url }}" alt="">
-
-                                            <!-- Favourite -->
-                                            <div class="product-favourite">
-                                                <a href="#" class="favme fa fa-heart"></a>
-                                            </div>
-                                        </div>
-
-                                        <!-- Product Description -->
-                                        <div class="product-description">
-                                            <span>{{ $item->subcategorie->categorie->name }}</span>
-                                            <a href="{{route('GetProduct',$item->id)}}">
-                                                <h6>{{ Str::limit($item->title, 15, '...') }}</h6>
-                                            </a>
-                                            <p class="product-price">${{ $item->price }}</p>
-
-                                            <!-- Hover Content -->
-                                            <div class="hover-content">
-                                                <!-- Add to Cart -->
-                                                <div class="add-to-cart-btn">
-                                                    <button onclick="AddToCart({{ $item->id }})" class="btn essence-btn">Add to Cart</button>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            @endforeach
-
-                        </div>
+                            <!-- Shop-Page /- -->
+                        @endforelse
                     </div>
-                    <!-- Pagination -->
-                    {{$products->links()}}
+                    {{ $products->links() }}
+                    <!-- Row-of-Product-Container /- -->
                 </div>
+                <!-- Shop-Right-Wrapper /- -->
             </div>
         </div>
-    </section>
-@endsection
-
-
-@section("scripts")
-    <script>
-        function AddToCart(id) {
-            $.ajax({
-                type: 'GET',
-                url: "AddToCart/" + id,
-                success: function(response) {
-                    // cardCount();
-                    // $('#success-message').show();
-                    // setTimeout(() => {
-                    //     $('#success-message').hide();
-                    // }, 3000);
-                    console.log('product added to cart')
-                },
-                error: function() {
-                    console.log('An error occurred .');
-                }
-            })
-        }
-    </script>
+    </div>
+    <!-- Shop-Page /- -->
 @endsection
